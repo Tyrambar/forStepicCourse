3933fd8d4752c2227119878adaf2a5f700426af6
Maxim <max28.ozymand@gmail.com>
2019-12-26 20:59:24 +0300

    Task 3; first Django, urls

--- etc/for_django.py
+++ etc/for_django.py
@@ -1,8 +1,5 @@
 #from cgi import parse_qs
 
-def django_app(environ, start_response):
-        start_response('200 OK', [('Content-Type', 'text/plain')])
-        return [bytes(res + '\n', 'ascii') for res in environ['QUERY_STRING'].split('&')]
-
 bind = '0.0.0.0:8000'
+pythonpath = '/home/box/web/ask'
 
