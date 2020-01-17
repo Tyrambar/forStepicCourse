89816c3505abf79c727b623b350b35ce61d6af6d
Tyrambar <max28.ozymand@gmail.com>
2020-01-17 13:31:59 +0000

    Task 4; second

--- init2.sh
+++ init2.sh
@@ -1,17 +1,19 @@
-sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt update && sudo apt install python3.6
-rm /usr/bin/python3
-ln -sf /usr/bin/python3.6 /usr/bin/python3
-rm /usr/bin/pydoc3
-ln -sf /usr/bin/pydoc3.6 /usr/bin/pydoc3
+sudo apt update
+sudo apt install python3.6
+sudo apt install python3.6-dev
+sudo unlink /usr/bin/python3
+sudo ln -sf /usr/bin/python3.6 /usr/bin/python3
 
 sudo wget https://bootstrap.pypa.io/ez_setup.py -O - | python3
 sudo pip3 install --force-reinstall setuptools
 
 sudo pip3 install django==2.1
 sudo pip3 install gunicorn==19.5
-sudo pip3 install mysqlclient==1.3.7
+sudo pip3 install mysqlclient==1.3.8
 #sudo apt install sqlite3
+
+#sudo apt install python-pip python-dev mysql-server libmysqlclient-dev
+#sudo apt install python3-dev
 
 #https://github.com/Tyrambar/forStepicCourse.git
 #INSTALLED_APPS = ['ask', 'qa', ..]
